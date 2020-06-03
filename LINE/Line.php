657a575674daec179df5a2e229676d2201c9ce9b
siv8510 <shibuuuu5@gmail.com>
2020-06-03 14:34:42 +0900

    resolve conflicts

--- LINE/Line.php
+++ LINE/Line.php
@@ -7,26 +7,26 @@ require_once __DIR__ . "/../Google/GoogleExecute.php";
 
 class Line
 {
-  function sendLine(string $send_msg): bool
-  {
-    $post_data['messages'][] = array(
-      'type' => 'text',
-      'text' => $send_msg
-    );
+    function sendLine(string $send_msg): bool
+    {
+        $post_data['messages'][] = array(
+            'type' => 'text',
+            'text' => $send_msg
+        );
 
-    $ch = curl_init('https://api.line.me/v2/bot/message/broadcast');
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/json; charser=UTF-8',
-      'Authorization: Bearer ' . LINE_ACCESS_TOKEN
-    ));
+        $ch = curl_init('https://api.line.me/v2/bot/message/broadcast');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json; charser=UTF-8',
+            'Authorization: Bearer ' . LINE_ACCESS_TOKEN
+        ));
 
-    $result = curl_exec($ch);
-    curl_close($ch);
+        curl_exec($ch);
+        curl_close($ch);
 
-    return true;
-  }
+        return true;
+      }
 }
